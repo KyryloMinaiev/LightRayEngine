eb4225c10bd3e7205f63351d79df4cb9ebf5f6c0
MrFlyingChipStudios <mrflyingchip@gmail.com>
2024-06-26 01:06:38 +0300

    Added file utils.

---
+++ Sources/Utils/FileUtils.h
@@ -0,0 +1,20 @@
+//
+// Created by MrFlyingChip on 25.06.2024.
+//
+
+#ifndef LIGHTRAYENGINE_FILEUTILS_H
+#define LIGHTRAYENGINE_FILEUTILS_H
+
+#include <string>
+
+class FileUtils {
+    public:
+        static bool TryLoadFile(const std::string& path, std::string& output);
+        static bool TryLoadFile(const std::string &path, std::vector<char>& output, int &dataLength);
+        static bool TrySaveFile(const std::string& path, const std::string& input);
+        static bool TrySaveFile(const std::string& path, const std::vector<char>& input, int dataLength);
+        static float GetLastFileChangeTime(const std::string& path);
+};
+
+
+#endif //LIGHTRAYENGINE_FILEUTILS_H

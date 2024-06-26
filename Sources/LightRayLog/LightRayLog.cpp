9e59e2e2f772793d172d0eff42b0e1c96cbbb9d7
Kyrylo Minaiev <mrflyingchip@gmail.com>
2024-06-27 01:29:22 +0300

    Logging system can log.

--- Sources/LightRayLog/LightRayLog.cpp
+++ Sources/LightRayLog/LightRayLog.cpp
@@ -1,5 +1,104 @@
 //
-// Created by Kirill Minaev on 26.06.2024.
+// Created by MrFlyingChip on 26.06.2024.
 //
 
 #include "LightRayLog.h"
+
+#include <iomanip>
+#include <sstream>
+
+namespace LightRay {
+    std::unique_ptr<LightRayLog> LightRayLog::s_instance;
+
+    LightRayLog *LightRayLog::GetOrCreateInstance() {
+        if(s_instance == nullptr){
+            s_instance = std::make_unique<LightRayLog>();
+        }
+
+        return s_instance.get();
+    }
+
+    void LightRayLog::SubscribeToLogUpdate(ILightRayLogUpdateSubscriber *logUpdateSubscriber) {
+        if(!logUpdateSubscriber){
+            return;
+        }
+
+        GetOrCreateInstance()->m_subscribers.push_back(logUpdateSubscriber);
+    }
+
+    void LightRayLog::UpdateSubscribers(const LogInfo &logInfo) {
+        for (auto subscriber : m_subscribers) {
+            if(subscriber){
+                subscriber->OnLogUpdated(logInfo);
+            }
+        }
+    }
+
+    void LightRayLog::UnsubscribeToLogUpdate(ILightRayLogUpdateSubscriber *logUpdateSubscriber) {
+        if(!logUpdateSubscriber){
+            return;
+        }
+
+        LightRayLog* log = GetOrCreateInstance();
+        auto it = std::find(log->m_subscribers.begin(), log->m_subscribers.end(), logUpdateSubscriber);
+        log->m_subscribers.erase(it);
+    }
+
+    template<typename T>
+    void LightRayLog::FormatString(std::ostringstream &os, const std::string &format, T value) {
+        size_t pos = format.find("{}");
+        if (pos != std::string::npos) {
+            os << format.substr(0, pos) << value;
+            FormatString(os, format.substr(pos + 2), "");
+        } else {
+            os << format;
+        }
+    }
+
+    template<typename T, typename... Args>
+    void LightRayLog::FormatString(std::ostringstream &os, const std::string &format, T value, Args... args) {
+        size_t pos = format.find("{}");
+        if (pos != std::string::npos) {
+            os << format.substr(0, pos) << value;
+            FormatString(os, format.substr(pos + 2), args...);
+        } else {
+            os << format;
+        }
+    }
+
+    template<typename... Args>
+    void LightRayLog::Log(const std::string &format, Args... args) {
+        GetOrCreateInstance()->LogInternal(LogLevel::Message, format, args...);
+    }
+
+    template<typename... Args>
+    void LightRayLog::LogWarning(const std::string &format, Args... args) {
+        GetOrCreateInstance()->LogInternal(LogLevel::Warning, format, args...);
+    }
+
+    template<typename... Args>
+    void LightRayLog::LogError(const std::string &format, Args... args) {
+        GetOrCreateInstance()->LogInternal(LogLevel::Error, format, args...);
+    }
+
+    template<typename... Args>
+    void LightRayLog::LogException(const std::string &format, Args... args) {
+        GetOrCreateInstance()->LogInternal(LogLevel::Exception, format, args...);
+    }
+
+    template<typename... Args>
+    void LightRayLog::LogInternal(LogLevel logLevel, const std::string &format, Args... args) {
+        LogInfo logInfo;
+
+        logInfo.logLevel = logLevel;
+        std::time_t now = std::time(nullptr);
+        logInfo.logTime = *std::localtime(&now);
+
+        std::ostringstream logMessage;
+        FormatString(logMessage, format, args...);
+        logInfo.log = logMessage.str();
+
+        m_logsList.push_back(logInfo);
+        UpdateSubscribers(logInfo);
+    }
+}
--- Sources/LightRayLog/LightRayLog.h
+++ Sources/LightRayLog/LightRayLog.h
@@ -1,13 +1,61 @@
 //
-// Created by Kirill Minaev on 26.06.2024.
+// Created by MrFlyingChip on 26.06.2024.
 //
 
 #ifndef LIGHTRAYENGINE_LIGHTRAYLOG_H
 #define LIGHTRAYENGINE_LIGHTRAYLOG_H
 
-namespace LightRay{
-    class LightRayLog {
+#include <string>
+#include <ctime>
+#include <memory>
+#include <vector>
 
+namespace LightRay{
+    enum class LogLevel{
+        Message,
+        Warning,
+        Error,
+        Exception
+    };
+
+    struct LogInfo{
+        LogLevel logLevel;
+        std::string log;
+        std::tm logTime;
+    };
+
+    class ILightRayLogUpdateSubscriber{
+    public:
+        virtual void OnLogUpdated(const LogInfo& logInfo) = 0;
+    };
+
+    class LightRayLog {
+    public:
+        template<typename... Args>
+        static void Log(const std::string& format, Args... args);
+        template<typename... Args>
+        static void LogWarning(const std::string& format, Args... args);
+        template<typename... Args>
+        static void LogError(const std::string& format, Args... args);
+        template<typename... Args>
+        static void LogException(const std::string& format, Args... args);
+
+        static void SubscribeToLogUpdate(ILightRayLogUpdateSubscriber* logUpdateSubscriber);
+        static void UnsubscribeToLogUpdate(ILightRayLogUpdateSubscriber* logUpdateSubscriber);
+    private:
+        static LightRayLog* GetOrCreateInstance();
+        template<typename... Args>
+        void LogInternal(LogLevel logLevel, const std::string& format, Args... args);
+        template<typename T>
+        void FormatString(std::ostringstream& os, const std::string& format, T value);
+        template<typename T, typename... Args>
+        void FormatString(std::ostringstream& os, const std::string& format, T value, Args... args);
+
+        void UpdateSubscribers(const LogInfo& logInfo);
+
+        std::vector<ILightRayLogUpdateSubscriber*> m_subscribers;
+        std::vector<LogInfo> m_logsList;
+        static std::unique_ptr<LightRayLog> s_instance;
     };
 }
 

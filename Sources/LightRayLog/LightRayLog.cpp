e9c5f3951dd4b4af72d791d3dbb7f10fa88d592e
MrFlyingChipStudios <mrflyingchip@gmail.com>
2024-06-27 00:15:07 +0300

    Added log files.

---
+++ Sources/LightRayLog/LightRayLog.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Kirill Minaev on 26.06.2024.
+//
+
+#include "LightRayLog.h"
---
+++ Sources/LightRayLog/LightRayLog.h
@@ -0,0 +1,14 @@
+//
+// Created by Kirill Minaev on 26.06.2024.
+//
+
+#ifndef LIGHTRAYENGINE_LIGHTRAYLOG_H
+#define LIGHTRAYENGINE_LIGHTRAYLOG_H
+
+namespace LightRay{
+    class LightRayLog {
+
+    };
+}
+
+#endif //LIGHTRAYENGINE_LIGHTRAYLOG_H

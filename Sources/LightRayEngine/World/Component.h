dc44e5d24aee3dbad651cc395f267c5d41e94c5e
Kyrylo Minaiev <mrflyingchip@gmail.com>
2024-11-17 01:03:35 +0200

    Component event functions.

--- Sources/LightRayEngine/World/Component.h
+++ Sources/LightRayEngine/World/Component.h
@@ -5,10 +5,37 @@
 #ifndef LIGHTRAYAPPLICATION_COMPONENT_H
 #define LIGHTRAYAPPLICATION_COMPONENT_H
 
+#include "Object/LightRayObject.h"
+
 namespace LightRayEngine {
+    class Entity;
 
-    class Component {
+    class Component : public LightRayObject {
+    public:
+        explicit Component(int objectID) : LightRayObject(objectID) {
+            m_entity = nullptr;
+        };
 
+        ~Component() override = default;
+
+        virtual void Awake() {};
+        virtual void Start() {};
+        virtual void Update() {};
+        virtual void LateUpdate() {};
+        virtual void OnEnable() {};
+        virtual void OnDisable() {};
+        virtual void OnDestroy() {};
+
+        void SetEntity(Entity *entity) {
+            m_entity = entity;
+        }
+
+        [[nodiscard]] Entity *GetEntity() const {
+            return m_entity;
+        }
+
+    private:
+        Entity *m_entity;
     };
 
 } // LightRayEngine

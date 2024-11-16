08bdebc6ea974f08d266a0cff9e62bf27024d5fc
Kyrylo Minaiev <mrflyingchip@gmail.com>
2024-11-17 01:03:13 +0200

    Entity has more fields to save.

--- Sources/LightRayEngine/World/Entity.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by MrFlyingChip on 10.11.2024.
-//
-
-#include "Entity.h"
-
-namespace LightRayEngine {
-} // LightRayEngine
--- Sources/LightRayEngine/World/Entity.h
+++ Sources/LightRayEngine/World/Entity.h
@@ -5,10 +5,14 @@
 #ifndef LIGHTRAYAPPLICATION_ENTITY_H
 #define LIGHTRAYAPPLICATION_ENTITY_H
 
+#include <string>
+
 namespace LightRayEngine {
-
     class Entity {
-
+    public:
+        int id;
+        bool enabled;
+        std::string name;
     };
 
 } // LightRayEngine
